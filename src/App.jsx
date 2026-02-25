2fe15233911a2365e2dfde29a6d9626600a5188f
AdityaNayak12 <adinayak18@gmail.com>
2026-02-25 23:21:59 +0530

    fix(app): wrong rendering of banner

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from './components/Navbar'
 import WatchList from './components/WatchList'
 import MoodSelector from './components/MoodSelector'
 import Movies from './components/Movies'
-import Banner from './components/Banner'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
@@ -17,7 +16,7 @@ function App() {
           <Route path="/mood" element={<MoodSelector />} />
         </Routes>
       </BrowserRouter>
-      <Banner/>
+
 
 
     </>
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Banner from "./Banner";
 
 function Movies(){
     return(
+    <>
+        <Banner/>
         <h1>Movies</h1>
+    </>
+        
     )
 }
 
